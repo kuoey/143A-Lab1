134aa13256ad28077a09f599ea307f5b91aa5eb0
sudney <46584496+sudney@users.noreply.github.com>
2021-03-06 21:13:44 -0800

    Update thread.h
    cleaned up

--- Pintos/pintos/src/threads/thread.h
+++ Pintos/pintos/src/threads/thread.h
@@ -5,10 +5,6 @@
 #include <list.h>
 #include <stdint.h>
 
-#ifdef VM
-#include "vm/page.h"
-#endif
-
 /* States in a thread's life cycle. */
 enum thread_status
   {
@@ -29,13 +25,11 @@ typedef int tid_t;
 #define PRI_MAX 63                      /* Highest priority. */
 
 /* A kernel thread or user process.
-
    Each thread structure is stored in its own 4 kB page.  The
    thread structure itself sits at the very bottom of the page
    (at offset 0).  The rest of the page is reserved for the
    thread's kernel stack, which grows downward from the top of
    the page (at offset 4 kB).  Here's an illustration:
-
         4 kB +---------------------------------+
              |          kernel stack           |
              |                |                |
@@ -57,22 +51,18 @@ typedef int tid_t;
              |               name              |
              |              status             |
         0 kB +---------------------------------+
-
    The upshot of this is twofold:
-
       1. First, `struct thread' must not be allowed to grow too
          big.  If it does, then there will not be enough room for
          the kernel stack.  Our base `struct thread' is only a
          few bytes in size.  It probably should stay well under 1
          kB.
-
       2. Second, kernel stacks must not be allowed to grow too
          large.  If a stack overflows, it will corrupt the thread
          state.  Thus, kernel functions should not allocate large
          structures or arrays as non-static local variables.  Use
          dynamic allocation with malloc() or palloc_get_page()
          instead.
-
    The first symptom of either of these problems will probably be
    an assertion failure in thread_current(), which checks that
    the `magic' member of the running thread's `struct thread' is
@@ -92,51 +82,28 @@ struct thread
     char name[16];                      /* Name (for debugging purposes). */
     uint8_t *stack;                     /* Saved stack pointer. */
     int priority;                       /* Priority. */
-    int original_priority;              /* Priority, before donation */
     struct list_elem allelem;           /* List element for all threads list. */
-    struct list_elem waitelem;          /* List element, stored in the wait_list queue */
-    int64_t sleep_endtick;              /* The tick after which the thread should awake (if the thread is in sleep) */
 
     /* Shared between thread.c and synch.c. */
-    struct list_elem elem;              /* List element, stored in the ready_list queue */
-
-    // needed for priority donations
-    struct lock *waiting_lock;          /* The lock object on which this thread is waiting (or NULL if not locked) */
-    struct list locks;                  /* List of locks the thread holds (for multiple donations) */
+    struct list_elem elem;              /* List element. */
 
 #ifdef USERPROG
     /* Owned by userprog/process.c. */
     uint32_t *pagedir;                  /* Page directory. */
-
-    // Project 2: file descriptors and process table
-    /* Owned by userprog/process.c and userprog/syscall.c */
-
-    struct process_control_block *pcb;  /* Process Control Block */
-    struct list child_list;             /* List of children processes of this thread,
-                                          each elem is defined by pcb#elem */
-
-    struct list file_descriptors;       /* List of file_descriptors the thread contains */
-
-    struct file *executing_file;        /* The executable file of associated process. */
-
-    uint8_t *current_esp;               /* The current value of the user program’s stack pointer.
-                                           A page fault might occur in the kernel, so we might
-                                           need to store esp on transition to kernel mode. (4.3.3) */
 #endif
-
-#ifdef VM
-    // Project 3: Supplemental page table.
-    struct supplemental_page_table *supt;   /* Supplemental Page Table. */
-
-    // Project 3: Memory Mapped Files.
-    struct list mmap_list;              /* List of struct mmap_desc. */
-#endif
-
-    // Project 4: CWD.
-    struct dir *cwd;
 
     /* Owned by thread.c. */
     unsigned magic;                     /* Detects stack overflow. */
+    int64_t wakeup; //added this attribute
+    /* ADD PRIORITY DONATION: struct thread */
+    int old_priority;                   /* orignal priority for donation purposes */
+    struct list locks_held;             /* list of locks currently held by this thread */
+    struct lock *wait_on_lock;          /* stores the lock on which it waits */
+
+
+    /* ADD MLFQS: struct thread */
+    int nice;
+    int recent_cpu;
   };
 
 /* If false (default), use round-robin scheduler.
@@ -147,7 +114,7 @@ extern bool thread_mlfqs;
 void thread_init (void);
 void thread_start (void);
 
-void thread_tick (int64_t tick);
+void thread_tick (void);
 void thread_print_stats (void);
 
 typedef void thread_func (void *aux);
@@ -155,8 +122,6 @@ tid_t thread_create (const char *name, int priority, thread_func *, void *);
 
 void thread_block (void);
 void thread_unblock (struct thread *);
-
-void thread_sleep_until (int64_t wake_tick);
 
 struct thread *thread_current (void);
 tid_t thread_tid (void);
@@ -171,11 +136,35 @@ void thread_foreach (thread_action_func *, void *);
 
 int thread_get_priority (void);
 void thread_set_priority (int);
-void thread_priority_donate(struct thread *, int priority);
 
 int thread_get_nice (void);
 void thread_set_nice (int);
 int thread_get_recent_cpu (void);
 int thread_get_load_avg (void);
+
+/* ADD MLFQ: function decralations */
+void calculate_recent_cpu(struct thread *t, void *aux UNUSED);
+void calculate_priority(struct thread *t, void *aux UNUSED);
+int get_ready_threads(void);
+int get_system_load_avg(void);
+void set_system_load_avg(int load);
+struct thread *get_idle_thread(void);
+
+//MLFQ functions
+static int p = 17, q = 14, f = 0;
+
+void init_f_value();
+ int convert_to_fixed_point(int n);
+ int covert_to_integer(int x);
+ int covert_to_integer_round(int x);
+ int add_fixed_point(int x, int y);
+ int subtract_fixed_point(int x, int y);
+ int add_fixed_and_integer(int x, int n);
+ int sub_fixed_and_integer(int x, int n);
+ int multiply_fixed_point(int x, int y);
+ int multiply_fixed_and_integer(int x, int n);
+ int divide_fixed_point(int x, int y);
+ int divide_fixed_and_integer(int x, int n);
+
 
 #endif /* threads/thread.h */
